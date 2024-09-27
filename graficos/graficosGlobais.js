0c45fbd3470fcf9f67f0fbff31d84dc4e6531078
sophiasoncin <110099928+sophiasoncin@users.noreply.github.com>
2024-09-27 08:53:04 -0300

    Add files via upload

--- graficos/graficosGlobais.js
+++ graficos/graficosGlobais.js
@@ -1,3 +1,14 @@
 const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
-console.log(url);
+async function vizualizaDadosGlobais(){
+   const res = await fetch(url);
+   const dados = await res.json();
+   console.log(dados);
+
+   const paragrafo = document.createElement('p')
+   paragrafo.classList.add('graficos-texto')
+   const container = document.getElementById('graficos.container')
+}
+
+vizualizaDadosGlobais();
+  
