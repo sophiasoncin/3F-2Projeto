db3883b36685ee9121e28ace74f7dcc05c0affae
sophiasoncin <110099928+sophiasoncin@users.noreply.github.com>
2024-10-03 09:44:17 -0300

    Add files via upload

--- graficos/graficosGlobais.js
+++ graficos/graficosGlobais.js
@@ -3,11 +3,18 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizaDadosGlobais(){
    const res = await fetch(url);
    const dados = await res.json();
+   const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+   const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+   const tempoMedio = dados.tempo_medio;
    console.log(dados);
 
    const paragrafo = document.createElement('p')
    paragrafo.classList.add('graficos-texto')
-   const container = document.getElementById('graficos.container')
+   const container = document.getElementById('graficos-container')
+   paragrafo.innerHTML = `O mundo tem ${totalPessoasMundo} bilhões de habitantes,dos quais
+   ${totalPessoasConectadas} bilhões estão conectadas em algumas rede social por um tempo médio de ${tempoMedio}.`
+   container.appendChild(paragrafo);
+
 }
 
 vizualizaDadosGlobais();
