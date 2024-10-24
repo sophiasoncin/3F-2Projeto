90f5af45c307a50a9ac2c2251abe6dfdec91e44e
sophiasoncin <110099928+sophiasoncin@users.noreply.github.com>
2024-10-24 09:27:56 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,28 +1,39 @@
 import { getCSS } from "./comum.js";
 async function quantidadeDeUsuarios(){
-const url =  "https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json"
-const res =  await fetch(url);
-const dados =  await res.json();
-const nomesDasRedes = Object.keys(dados);
-const quantidadeDeUsuarios = Object.values(dados);
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url);
+    const dados = await res.json();
+    const nomeDasRedes = Object.keys(dados);
+    const quantidadeDeUsuarios = Object.values(dados);
 
-const infos = [
-    {
-        x: nomesDasRedes,
-        y: quantidadeDeUsuarios,
-        type: 'bar', 
-        marker:{
-            color: getCSS("--cor-primaria")
+    const infos = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeDeUsuarios,
+            type: 'bar',
+            marker:{
+                color: getCSS('--cor-primaria')
+            }
+        }
+    ]
+    const layout = {
+        plot_bgcolor: getCSS("--cor-de-fundo"),
+        paper_bgcolor: getCSS("--cor-de-fundo"),
+        title:{
+            text: "Redes sociais com mais usuário no mundo",
+            x: 0,
+            font:{
+                color: getCSS("--cor-primaria"),
+                family:getCSS("--font"),
+                size: 30
+            }
         }
     }
-]
 
-const grafico = document.createElement('div');
-grafico.className = "grafico";
-document.getElementById('graficos-container').appendChild(grafico);
-Plotly.newPlot(grafico,infos);    
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico,infos,layout);
 }
 
-quantidadeDeUsuarios(); 
-
-
+quantidadeDeUsuarios();
